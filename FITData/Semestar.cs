96b1c7d23c9d6b50341700abd12bbba90bbcf47d
Hamza Nesimovic <you@example.com>
2023-12-20 01:30:51 +0100

    .

--- FITData/Semestar.cs
+++ FITData/Semestar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace FITData
 {
     public class Semestar
     {
+        
         public int Id { get; set; }
         public string Oznaka { get; set; }
         public string Opis { get; set; }
